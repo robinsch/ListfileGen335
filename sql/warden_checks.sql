31c1866803279133cbebc3c8055172253faf5a60
robinsch <hello@robinsch.com>
2023-09-08 18:51:58 +0200

    Added waren_checks DLL scans to allow server-side detection

--- sql/warden_checks.sql
+++ sql/warden_checks.sql
@@ -1,2 +1,4 @@
 INSERT INTO `warden_checks` (`type`, `str`, `address`, `length`, `comment`, `data`, `result`) VALUES (243, NULL, 4404625, NULL, 'Code injection at 0x433591', NULL, 0x3FEC);
 INSERT INTO `warden_checks` (`type`, `str`, `address`, `length`, `comment`, `data`, `result`) VALUES (243, NULL, 4344657, NULL, 'Code injection at 0x424B51', NULL, 0x3FEC);
+INSERT INTO `warden_checks` (`type`, `str`, `comment`) VALUES (217, 'LISTFILEGEN.DLL', 'ListfileGen - injected dll');
+INSERT INTO `warden_checks` (`type`, `str`, `comment`) VALUES (217, 'MPQ_LISTFILE_335.DLL', 'mpq_listfile_335 - injected dll');
