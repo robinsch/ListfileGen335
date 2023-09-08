c7a3722e97bce8a8dee7a3dc8a11dc5b7753667e
robinsch <hello@robinsch.com>
2023-09-08 18:48:38 +0200

    Added waren_checks to allow server-side detection

---
+++ sql/warden_checks.sql
@@ -0,0 +1,2 @@
+INSERT INTO `warden_checks` (`type`, `str`, `address`, `length`, `comment`, `data`, `result`) VALUES (243, NULL, 4404625, NULL, 'Code injection at 0x433591', NULL, 0x3FEC);
+INSERT INTO `warden_checks` (`type`, `str`, `address`, `length`, `comment`, `data`, `result`) VALUES (243, NULL, 4344657, NULL, 'Code injection at 0x424B51', NULL, 0x3FEC);
